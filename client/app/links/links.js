7f7669c3821a2ed86caeaebfa8908d02969762a9
archcorsair <archcorsair@gmail.com>
2016-10-15 09:50:28 -0700

    Links displaying and redirecting

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,9 +1,10 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, $location, Links) {
+.controller('LinksController', function($scope, $location, Links) {
   $scope.data = {};
   Links.getAll().then(function(links) {
     $scope.data.links = links;
   });
   $location.path('/links');
+
 });
